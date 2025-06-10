62083aede71cb8b52aab312e9842a1222aa6f1dc
isochikuwa <isochikuwa0522@gmail.com>
2025-06-11 08:07:40 +0900

    分割コンパイルするように修正

---
+++ 9cc.h
@@ -0,0 +1,71 @@
+#include <stdbool.h>
+
+typedef enum {
+  TK_RESERVED,  // 記号
+  TK_NUM,       // 数値
+  TK_EOF,       // 入力の終わりを示すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+  TokenKind kind; // トークンの型
+  Token *next;    // 次の入力トークン
+  int val;        // kindがTK_NUMの場合、その数値
+  char *str;      // トークン文字列
+  int len;        // トークンの長さ
+};
+
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_EQ,  // ==
+  ND_NEQ, // !=
+  ND_LT,  // <
+  ND_LTE, // <=
+  ND_GT,  // >
+  ND_GTE, // >=
+  ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+  NodeKind kind;  // ノードの型
+  Node *lhs;      // 左辺
+  Node *rhs;      // 右辺
+  int val;        // kindがND_NUMの場合のみ使う
+};
+
+Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
+Node *mul();
+Node *unary();
+Node *primary();
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+void error_at(char *loc, char *fmt, ...);
+bool consume(char *op);
+void expect(char *op);
+int expect_number();
+bool at_eof();
+Token *new_token(TokenKind kind, Token *cur, char *str, int len);
+Token *tokenize(char *p);
+Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
+Node *mul();
+Node *unary();
+Node *primary();
+void gen(Node *node);
+
+// 入力プログラム
+extern char *user_input;
+// 現在着目しているトークン
+extern Token *token;
---
+++ tokenize.c
@@ -0,0 +1,91 @@
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include "9cc.h"
+
+// 次のトークンが期待している記号のときは、トークンを1つ読み進めて
+// 真を返す。それ以外は偽を返す。
+bool consume(char *op) {
+  if (token->kind != TK_RESERVED ||
+      strlen(op) != token->len ||
+      memcmp(token->str, op, token->len)) {
+    return false;
+  }
+
+  token = token->next;
+  return true;
+}
+
+// 次のトークンが期待している記号のときは、トークンを1つ読み進める。
+// それ以外はエラーを報告する。
+void expect(char *op) {
+  if (token->kind != TK_RESERVED ||
+      strlen(op) != token->len ||
+      memcmp(token->str, op, token->len)) {
+    error_at(token->str, "'%c'ではありません", op);
+  }
+
+  token = token->next;
+}
+
+// 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
+// それ以外はエラーを報告する。
+int expect_number() {
+  if(token->kind != TK_NUM) {
+    error_at(token->str, "数ではありません");
+  }
+
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof() {
+  return token->kind == TK_EOF;
+}
+
+// 新しいトークンを作成してcurに繋げる
+Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  tok->len = len;
+  cur->next = tok;
+  return tok;
+}
+
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    // 空白文字をスキップ
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (!(strncmp(p, "==", 2) && strncmp(p, "!=", 2) && strncmp(p, ">=", 2) && strncmp(p, "<=", 2))) {
+      cur = new_token(TK_RESERVED, cur, p, 2);
+      p += 2;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')' || *p == '>' || *p == '<') {
+      cur = new_token(TK_RESERVED, cur, p++, 1);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p, 0);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error_at(cur->str, "トークナイズできません");
+  }
+
+  new_token(TK_EOF, cur, p, 0);
+  return head.next;
+}
